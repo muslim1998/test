6a4ccddd8983fcd7afd5a8cb2460f793973dfc54
Muslim <muslim_k_m_98@mail.ru>
2023-06-05 21:18:12 +0600

    add

---
+++ script.js
@@ -0,0 +1,35 @@
+let button = document.getElementById('finish');
+
+button.addEventListener('click', checkTest)
+
+function checkTest() {
+    let result = 0;
+
+    let a1 = document.getElementById('q1').value;
+    if (a1 === '4') {
+        result++;
+    }
+
+    let a2 = document.getElementById('q2').value;
+    if (a2 === '30') {
+        result++;
+    }
+
+    let a3 = document.getElementById('q3').value;
+    if (a3 === '10') {
+        result++;
+    }
+
+    let a4 = document.getElementById('q4').value;
+    if (a4 === '36') {
+        result++;
+    }
+
+    let a5 = document.getElementById('q5').value;
+    if (a5 === '1') {
+        result++;
+    }
+
+    alert('Количество правильныx ответов: ' + result);
+};
+
